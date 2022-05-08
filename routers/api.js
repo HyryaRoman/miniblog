21e82389dec225af5661c74ee62c4fe439415519
HyryaRoman <hyrya.roman@gmail.com>
2022-05-08 21:18:51 +0300

    added ability to post posts

--- routers/api.js
+++ routers/api.js
@@ -1,13 +1,94 @@
 const express = require('express')
 const bp = require('body-parser')
 const router = express.Router()
+const mysql = require('promise-mysql')
+
+const createPool = async () => {
+    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+
+    // Establish a connection to the database
+    return mysql.createPool({
+        user: process.env.DB_USER, // e.g. 'my-db-user'
+        password: process.env.DB_PASS, // e.g. 'my-db-password'
+        database: process.env.DB_NAME, // e.g. 'my-database'
+        // If connecting via unix domain socket, specify the path
+        socketPath: process.env.INST_UNIT_SOCK,
+        // Specify additional properties here.
+        
+        // 'connectionLimit' is the maximum number of connections the pool is allowed
+        // to keep at once.
+        connectionLimit: 5,
+
+        // 'connectTimeout' is the maximum number of milliseconds before a timeout
+        // occurs during the initial connection to the database.
+        connectTimeout: 10000, // 10 seconds
+
+        // 'acquireTimeout' is the maximum number of milliseconds to wait when
+        // checking out a connection from the pool before a timeout error occurs.
+        acquireTimeout: 10000, // 10 seconds
+
+        // 'waitForConnections' determines the pool's action when no connections are
+        // free. If true, the request will queued and a connection will be presented
+        // when ready. If false, the pool will call back with an error.
+        waitForConnections: true, // Default: true
+
+        // 'queueLimit' is the maximum number of requests for connections the pool
+        // will queue at once before returning an error. If 0, there is no limit.
+        queueLimit: 0, // Default: 0
+  });
+};
+
+const ensureSchema = async pool => {
+    await pool.query(
+        `CREATE TABLE IF NOT EXISTS posts (
+        post_id int NOT NULL,
+        post_title tiny_text NOT NULL,
+        post_desc tiny_text NOT NULL,
+        post_text mediumtext NOT NULL,
+        PRIMARY KEY (post_id)`)
+}
+
+const createPoolAndEnsureSchema = async () = {
+    await createPool().then(async pool => {
+        await ensureSchema(pool)
+        return pool
+    }).catch(err => { throw err })}
+
+let pool
+
+router.use(async (req, res, next) = {
+    if (pool) {
+        return next()
+    }
+    try {
+        pool = await createPoolAndEnsureSchema()
+        next()
+    } catch (err) {
+        return next(err)
+    }
+})
 
 router.use(bp.json())
 router.use(bp.urlencoded({ extended: true }))
 
 router.post("/api/post", (req, res, next) => {
-    res.redirect("/view/001")
+    // res.redirect("/view/001")
     const data = req.body
+    const pname = data.post_name
+    const pdesc = data.post_text.substring(0, 255)
+    const ptext = data.post_text
+    pool = pool || await createPoolAndEnsureSchema()
+    try {
+        const postCreationQuery = pool.query(
+            `INSERT INTO posts (post_title, post_desc, post_text)
+            OUTPUT Inserted.post_id
+            VALUES (${pname}, ${pdesc}, ${ptext})`)
+
+        const pid = await postCreationQuery
+        res.redirect(`/view/${pid}`)
+    } catch (err) {
+        res.status(500).send('Unable to create post. Hope you have saved it somewhere.').end()
+    }
 })
 
 module.exports = router
