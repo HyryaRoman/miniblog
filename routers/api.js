1481a754fe3203b5018ae2b469d46f1c2bb9144b
HyryaRoman <hyrya.roman@gmail.com>
2022-05-08 21:38:58 +0300

    fix promise-mysql and async

--- routers/api.js
+++ routers/api.js
@@ -1,11 +1,10 @@
-const express = require('express')
-const bp = require('body-parser')
-const router = express.Router()
-const mysql = require('promise-mysql')
+const express = require('express');
+const bp = require('body-parser');
+const router = express.Router();
+const mysql = require('promise-mysql');
+
 
 const createPool = async () => {
-    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
-
     // Establish a connection to the database
     return mysql.createPool({
         user: process.env.DB_USER, // e.g. 'my-db-user'
@@ -45,50 +44,55 @@ const ensureSchema = async pool => {
         post_title tiny_text NOT NULL,
         post_desc tiny_text NOT NULL,
         post_text mediumtext NOT NULL,
-        PRIMARY KEY (post_id)`)
-}
+        PRIMARY KEY (post_id)`);
+};
 
-const createPoolAndEnsureSchema = async () = {
-    await createPool().then(async pool => {
-        await ensureSchema(pool)
-        return pool
-    }).catch(err => { throw err })}
+const createPoolAndEnsureSchema = async () => {
+    await createPool().then(
+        async pool => {
+            await ensureSchema(pool);
+            return pool;
+        }
+    ).catch(err => {
+        throw err;
+    });
+};
 
-let pool
+let pool;
 
-router.use(async (req, res, next) = {
+router.use(async (req, res, next) => {
     if (pool) {
-        return next()
+        return next();
     }
     try {
-        pool = await createPoolAndEnsureSchema()
-        next()
+        pool = await createPoolAndEnsureSchema();
+        next();
     } catch (err) {
-        return next(err)
+        return next(err);
     }
-})
+});
 
-router.use(bp.json())
-router.use(bp.urlencoded({ extended: true }))
+router.use(bp.json());
+router.use(bp.urlencoded({ extended: true }));
 
-router.post("/api/post", (req, res, next) => {
-    // res.redirect("/view/001")
-    const data = req.body
-    const pname = data.post_name
-    const pdesc = data.post_text.substring(0, 255)
-    const ptext = data.post_text
-    pool = pool || await createPoolAndEnsureSchema()
+router.post("/api/post", async (req, res, next) => {
+    // res.redirect("/view/001");
+    const data = req.body;
+    const pname = data.post_name;
+    const pdesc = data.post_text.substring(0, 255);
+    const ptext = data.post_text;
+    pool = pool || (await createPoolAndEnsureSchema());
     try {
         const postCreationQuery = pool.query(
             `INSERT INTO posts (post_title, post_desc, post_text)
             OUTPUT Inserted.post_id
-            VALUES (${pname}, ${pdesc}, ${ptext})`)
+            VALUES (${pname}, ${pdesc}, ${ptext})`);
 
-        const pid = await postCreationQuery
-        res.redirect(`/view/${pid}`)
+        const pid = await postCreationQuery;
+        res.redirect(`/view/${pid}`);
     } catch (err) {
-        res.status(500).send('Unable to create post. Hope you have saved it somewhere.').end()
+        res.status(500).send('Unable to create post. Hope you have saved it somewhere.').end();
     }
-})
+});
 
-module.exports = router
+module.exports = router;
