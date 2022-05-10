9a4bb57662e991032af26ccca5ab472cb5df4321
HyryaRoman <hyrya.roman@gmail.com>
2022-05-10 22:22:01 +0300

    remove firebase

--- index.js
+++ index.js
@@ -2,26 +2,6 @@ const express = require('express')
 const app = module.exports = express()
 const path = require('path')
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAMwVQovJOraI-hE2P8kNPh1Yz-H9Sy8fU",
-  authDomain: "miniblog-349615.firebaseapp.com",
-  projectId: "miniblog-349615",
-  storageBucket: "miniblog-349615.appspot.com",
-  messagingSenderId: "302805054463",
-  appId: "1:302805054463:web:6b728120cb5e785050edca",
-  measurementId: "G-VC3SNPM322"
-};
-
-// Initialize Firebase
-const app2 = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app2);
-
 const port = process.env.PORT || 8080
 
 app.engine('.html', require('ejs').__express)
